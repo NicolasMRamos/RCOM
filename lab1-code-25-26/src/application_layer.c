50e762b77356b35a991395206a3c206b57defa96
Rachel-zz <raquelgfernandes88@gmail.com>
2025-10-30 17:02:13 +0000

    irrelevant

--- lab1-code-25-26/src/application_layer.c
+++ lab1-code-25-26/src/application_layer.c
@@ -28,33 +28,7 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
         exit(EXIT_FAILURE);
     }
 
-    unsigned char buffer1[5] = {0x01,0x02,0x03,0x04,0x05};
-    unsigned char buffer2[5] = {0x06,0x07,0x08,0x09, 0x10};
-
-    if(configs.role == LlTx){
-        ret = llwrite(buffer1, 5);
-        if (ret == -1) {
-            printf("Error: llwrite failed\n");
-            exit(EXIT_FAILURE);
-        }
-        ret = llwrite(buffer2, 5);
-        if (ret == -1) {
-            printf("Error: llwrite failed\n");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        unsigned char packet[MAX_PAYLOAD_SIZE];
-        ret = llread(packet);
-        if (ret == -1) {
-            printf("Error: llread failed\n");
-            exit(EXIT_FAILURE);
-        }
-        ret = llread(packet);
-        if (ret == -1) {
-            printf("Error: llread failed\n");
-            exit(EXIT_FAILURE);
-        }
-    }
+    
 
     ret = llclose();
     if (ret == -1) {
